671c45b5c571ef91bca5d2e422280c4bc8a344b2
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-09-29 10:57:37 -0500

    Ejercicio10 Guia3

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
-let con=1
-let cref=Number(prompt("Digite los creditos de la materia de fundamentos:"));
-let creb=Number(prompt("Digite los creditos de la materia bd:"));
-let cree=Number(prompt("Digite los creditos de la materia de etica:"));
-let can=Number(prompt("Digite la cantidad de estudiantes:"));
+let veh=Number(prompt("Ingrese la cantidad de vehiculos"));
+let tur=0;
+let pert=0;
+let aut=0;
+let cam=0;
+let mot=0;
+let con=1;
+let per;
+while (con<=veh){
+    let tip=prompt("tipo de vehiculo que pasa(turismo,autobus,camion o motocicleta):")
+    con+=1
+    
+    if(tip=="turismo"){
+       per=Number(prompt("cantidad de personas"))
+       tur+=1
+       pert=pert+pert
+    }
+      
+    else if(tip=="autobus:"){
+        aut+=1
+    }
+      
+    else if(tip=="camion"){
+        cam+=1
+    }
+      
+    else{
+        mot+=1
+    }
+      
 
-while (con<=can){
-    let calf=Number(prompt("Digite la calificacion de la materia de funadamentos:"));
-    let calb=Number(prompt("Digite la calificacion de la materia de bd:"));
-    let cale=Number(prompt("Digite la calificacion de la materia de etica"));
-    
-    let mat1=calf*cref;
-    let mat2=calb*creb;
-    let mat3=cale*cree;
-    /*suma de creditos lo siguiente*/
-    let cre=cref+creb+cree;
-    
-    /*#sumamos todas las materias divido en el total de sus creditos*/
-    let pon=(mat1+mat2+mat3)/cre;
-    alert("su promedio ponderado es de:"+pon);
-    con+=1 
+  alert("Cantidad de autobuses"+aut)
+  alert("Cantidad de camiones"+cam)
+  alert("Cantidad de motocicletas"+mot)
+  alert("cantidad de turismo"+ur)
+  alert("cantidad de personas en vehiculos de turismo"+pert)
 }
-  
+
+             
   
   /* Datos de salida*/
  
