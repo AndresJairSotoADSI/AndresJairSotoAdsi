64067455e854f12dbdbde26892483587a870e4ea
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-09-29 11:37:17 -0500

    Ejercicio 1 Guia4

--- main.js
+++ main.js
@@ -1,45 +1,20 @@
-let veh=Number(prompt("Ingrese la cantidad de vehiculos"));
-let tur=0;
-let pert=0;
-let aut=0;
-let cam=0;
-let mot=0;
-let con=1;
-let per;
-while (con<=veh){
-    let tip=prompt("tipo de vehiculo que pasa(turismo,autobus,camion o motocicleta):")
-    con+=1
-    
-    if(tip=="turismo"){
-       per=Number(prompt("cantidad de personas"))
-       tur+=1
-       pert=pert+pert
-    }
-      
-    else if(tip=="autobus:"){
-        aut+=1
-    }
-      
-    else if(tip=="camion"){
-        cam+=1
-    }
-      
-    else{
-        mot+=1
-    }
-      
+function escribirTablaMultiplicar(j){
 
-  alert("Cantidad de autobuses"+aut)
-  alert("Cantidad de camiones"+cam)
-  alert("Cantidad de motocicletas"+mot)
-  alert("cantidad de turismo"+ur)
-  alert("cantidad de personas en vehiculos de turismo"+pert)
+	document.write("<h2>Tabla de multiplicar del "+ j +"</h2>");
+
+	document.write("<ul>");
+	//i++ significa i=i+1;
+
+	for(i = 1;i<=10;i++){
+		document.write("<li>");
+		document.write(j + "x " + i + "= " + j * i);
+		document.write("</li>");
+	}
+
+	document.write("</ul>");
 }
 
-             
-  
-  /* Datos de salida*/
- 
+escribirTablaMultiplicar(5);
 
 
 
