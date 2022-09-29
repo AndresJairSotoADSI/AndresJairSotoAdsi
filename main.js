effaa8918b9019674d7956ea3a4c49a5ab7b163e
AndresJairSotoADSI <ynfinityink@gmail.com>
2022-09-29 08:33:40 -0500

    Ejercicio3 Guia3

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-
-let num1=Number(prompt("ingresar el numero 1 a evaluar:"));
-let num2=Number(prompt("ingresar el numero 2"));
+let cantidadepersonas=Number(prompt("ingresar la cantidad de personas:"));
 let cont=1;
-
-while (cont<=10){
-   
-    let suma=(num1+num2);
-    let resta=(num1-num2);
-    let multi=(num1*num2);
-    let divi=(num1/num2);
-    alert(num1+"+"+num2+"="+suma);
-    alert(num1+"-"+num2+"="+resta);
-    alert(num1+"*"+num2+"="+multi);
-    alert(num1+"/"+num2+"="+divi);
+while (cont<=cantidadepersonas){
+    let nombre=String(prompt("escriba el nombre:"));
+    let apellido=String(prompt("escriba el apellido:"));
+    let edad=Number(prompt("numero de edad:"));
+    let notapro=Number(prompt("ingresar le nota promedio:"));
+    let datospersonales=("nombre"+nombre+"apellido"+apellido+"edad"+edad+"notanotapro"+notapro);
+    alert("los datos por personas son:"+datospersonales);
     cont+=1;
 }
-alert("Fin del proceso") ;
+alert("Fin del proceso");  
+
 
 
 
